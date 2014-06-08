fd42d795d5748a338c0ab9cb9192db76dc5bb02e
MathiasB <bekemathias@gmail.com>
2014-06-08 12:23:45 +0200

    Checking if the URL could be fetched

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+/*
+PHP script for downloading remote fonts in a CSS file.
+
+Author: Mathias Beke
+Data: June 2014
+*/
 
 require_once(__DIR__ . '/lib/Sabberworm/CSS/Property/AtRule.php');
 require_once(__DIR__ . '/lib/Sabberworm/CSS/Property/Charset.php');
@@ -28,13 +34,13 @@ require_once(__DIR__ . '/lib/Sabberworm/CSS/Value/URL.php');
 require_once(__DIR__ . '/lib/Sabberworm/CSS/Parser.php');
 
 
-$sMyId = "#my_id";
-$sText = file_get_contents('style.css');
-$oParser = new Sabberworm\CSS\Parser($sText);
-$oCss = $oParser->parse();
+$text = file_get_contents('style.css');
+$parser = new Sabberworm\CSS\Parser($text);
+$css = $parser->parse();
 
 
-foreach ($oCss->getAllRuleSets() as $block) {
+foreach ($css->getAllRuleSets() as $block) {
+	
 	$font_src = $block->getRules("src");
 	$font_family = $block->getRules("font-family");
 	$font_weight = $block->getRules("font-weight");
@@ -42,11 +48,20 @@ foreach ($oCss->getAllRuleSets() as $block) {
 	
 	if( 1 == sizeof($font_src) and 1 == sizeof($font_family) and 1 == sizeof($font_weight) and 1 == sizeof($font_style) ) {
 		
+		//Extract font metadata
+		$font_family = $font_family[0]->getValue();
+		$font_weight = $font_weight[0]->getValue();
+		$font_style = $font_style[0]->getValue();
+		
+		//Replace spaces with a dash
+		$font_family = implode( '-', explode(' ', $font_family) );
+		
+		//Remove starting/ending quote
+		$font_family = substr($font_family, 1, sizeof($font_family)-2);
+		
 		//Compose the file name for the font
-		// font-name + font-weight
-		$file_name = implode( '-', explode(' ', $font_family[0]->getValue()) );
-		$file_name = substr($file_name, 1, sizeof($file_name)-2);
-		$file_name = $file_name . '-' . $font_weight[0]->getValue() . '-' . $font_style[0]->getValue();
+		// font-name + font-weight + font-style
+		$file_name = $font_family . '-' . $font_weight . '-' . $font_style;
 		$file_name = $file_name . '.woff';
 		$file_name = __DIR__ . '/font/' . $file_name;
 		
@@ -54,20 +69,31 @@ foreach ($oCss->getAllRuleSets() as $block) {
 		foreach ($font_src[0]->getValue()->getListComponents()[0]->getListComponents() as $component) {
 		
 			if ( get_class($component) == 'Sabberworm\CSS\Value\URL' ) {
+				
 				$url = $component->getURL();
 				$url = substr($url, 1, sizeof($url)-2);
-				echo $url;
 				
-				file_put_contents( $file_name , file_get_contents($url));
+				$downloaded_font = @file_get_contents($url);
+				
+				//Check if the url could be downloaded
+				if($downloaded_font === false) {
+					echo "Could not download $font_family ($font_style, $font_weight) from '$url'";
+					echo '<hr>';
+				}
+				else {
+					file_put_contents( $file_name , $downloaded_font);
+					echo "Downloaded $font_family ($font_style, $font_weight) from '$url'";
+					echo '<hr>';
+				}
+				
+				break; //No need to search further
+				
 			}
 			
 		}
-		
-		echo "<hr><br>";
 	}
 	
 }
 
 
 ?>
-<pre><?php echo $oCss; ?></pre>
